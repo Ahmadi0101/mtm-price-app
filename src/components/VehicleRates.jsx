b245576f39437e197367b322faf1eb869ffea7bc
Ahmadi0101 <ahmadahmadig072@gmali.com>
2026-09-16 17:08:51 +0430

    update

--- src/components/VehicleRates.jsx
+++ src/components/VehicleRates.jsx
@@ -394,7 +394,7 @@ export default function VehicleRates({ appData, selectedVehicle, onClose, onSele
 
           <div className="vehicle-rates-title">
             <div>
-              <h2>نرخ گمرک موتر</h2>
+              <h2>نرخ گمرک موتر</h2>طططط
 
               <p>تعداد نرخ های موجود: {vehicles.length}</p>
             </div>
