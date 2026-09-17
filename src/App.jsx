0717301b2411d3337fa02a8b01b5b891f0b0056b
Ahmadi0101 <ahmadahmadig072@gmali.com>
2026-09-17 15:53:14 +0430

    IMOGI

--- src/App.jsx
+++ src/App.jsx
@@ -585,7 +585,7 @@ const [popupCloseKey, setPopupCloseKey] = useState(0);
                               {/* SHIP */}
 
                               <div>
-                                <span>USA(🇦🇫) → TR(🇹🇷)</span>
+                                <span>🇺🇸 → 🇹🇷</span>
 
                                 <strong>${Number(port.ship || 0).toLocaleString()}</strong>
                               </div>
@@ -593,7 +593,7 @@ const [popupCloseKey, setPopupCloseKey] = useState(0);
                               {/* HERAT */}
 
                               <div>
-                                <span>TR() → AF(🇦🇫)</span>
+                                <span>🇹🇷 → 🇦🇫</span>
 
                                 <strong>${Number(port.herat || 0).toLocaleString()}</strong>
                               </div>
